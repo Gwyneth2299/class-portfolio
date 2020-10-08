1c5a49f6846355033154d631e1134dddeba47602
Gwyneth2299 <70980984+Gwyneth2299@users.noreply.github.com>
2020-10-08 18:29:12 -0400

    Update sketch.js

--- self-portrait_v3/sketch.js
+++ self-portrait_v3/sketch.js
@@ -1,10 +1,10 @@
 /*
-	self portrait v2
+	self portrait v3
 	by gwen
 */
 
 // global scope
-var x = 200; // statement
+var x; // statement
 var y = 200;
 
 let r;
@@ -20,7 +20,8 @@ let col = {
 
 
 function setup() {
-	createCanvas(640, 360);
+	createCanvas(640, 360)
+   x = width - 380;
 }
 
 function draw () {
@@ -37,61 +38,69 @@ function draw () {
    //Hair
   noStroke()
   fill(32, 19, 19);
-  arc(260, 270, 300, 460, QUARTER_PI + HALF_PI, QUARTER_PI, OPEN);
+  arc(x + 60, y + 70, 300, 460, QUARTER_PI + HALF_PI, QUARTER_PI, OPEN);
 
   //Face 
   noStroke()
   fill('#ffcb9a');
-  ellipse(260, 210, 200, 260);
+  ellipse(x + 60, y + 10, 200, 260);
 
   //Ears
-  arc(150, 235, 40, 60, 0, PI + HALF_PI, PI + HALF_PI, OPEN);
-  arc(370, 235, 40, 60, 0, PI + PI, HALF_PI + PI);
+  arc(x - 50, y + 35, 40, 60, 0, PI + HALF_PI, PI + HALF_PI, OPEN);
+  arc(x + 170, y + 35, 40, 60, 0, PI + PI, HALF_PI + PI);
 
   //Bang
   noStroke()
   fill(32, 19, 19);
-  arc(310, 130, 130, 105, 0, PI + HALF_PI, CHORD);
-  arc(180, 110, 140, 148, 0, 0.75 * Math.PI);
-  arc(195, 80, 180, 120, 0, 0.75 * Math.PI);
+  arc(x + 110, y - 70, 130, 105, 0, PI + HALF_PI, CHORD);
+  arc(x - 20, y -90, 140, 148, 0, 0.75 * Math.PI);
+  arc(x - 5, y - 120, 180, 120, 0, 0.75 * Math.PI);
 
   //Eyebrows
   stroke(21, 19, 19);
   strokeWeight(3);
   noFill();
-  arc(210, 190, 45, 15, PI, TWO_PI, OPEN);
-  arc(310, 190, 45, 15, PI, TWO_PI, OPEN);
+  arc(x + 10, y - 10, 45, 15, PI, TWO_PI, OPEN);
+  arc(x + 110, y - 10, 45, 15, PI, TWO_PI, OPEN);
 
   //Eyeshadow
   stroke(62, 19, 19, 220);
   strokeWeight(6);
-  arc(207, 225, 55, 35, PI, TWO_PI, OPEN);
-  arc(315, 225, 55, 35, PI, TWO_PI, OPEN);
+  arc(x + 7, y + 25, 55, 35, PI, TWO_PI, OPEN);
+  arc(x + 115, y + 25, 55, 35, PI, TWO_PI, OPEN);
 
   //Eyes
   strokeWeight(1)
   fill(32, 19, 19);
-  ellipse(220, 225, 30, 30);
-  ellipse(325, 225, 30, 30);
+  ellipse(x + 20, y + 25, 30, 30);
+  ellipse(x + 125, y + 25, 30, 30);
 
   //Eyeballs
   noStroke()
   fill(255)
-  ellipse(217, 220, 8, 8);
-  ellipse(317, 220, 8, 8);
+  ellipse(x + 17, y + 20, 8, 8);
+  ellipse(x + 117, y + 20, 8, 8);
 
   //Nose
   fill('#fdbc85')
-  triangle(260, 230, x + 70, y + 80, x + 50, y + 80);
+  triangle(x + 60, y + 30, x + 70, y + 80, x + 50, y + 80);
 
   //Mouth
   fill('#f28865');
-  arc(260, 298, 49, 35, 0, PI);
+  arc(x + 60, y + 98, 49, 35, 0, PI);
 
 
   //Neck
   rectMode(CENTER)
   fill('#ffcb9a')
-  rect(260, 350, 50, 55, 20);
+  rect(x + 60, y + 150, 50, 55, 20);
+
+  // text
+  textSize(30);
+  fill('plum');
+  stroke(255);
+  strokeWeight(2); 
+  textAlign(CENTER)
+  text('Move the mouse to paint her background', width / 2, height - 300);
 
 }
