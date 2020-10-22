6992cd8947e2fbffc927b5c79d4b0760073f94a7
Gwyneth2299 <70980984+Gwyneth2299@users.noreply.github.com>
2020-10-22 16:21:09 -0400

    update sketch.js

---
+++ meme_v3/sketch.js
@@ -0,0 +1,52 @@
+/*
+	meme v1
+	by Gwen
+	text + images
+*/
+
+//var memeImage;
+
+var x = 0;
+var y = 0;
+var xSpeed = 3;
+var ySpeed = 3;
+
+function preload() {
+	memeImage = loadImage('meme.jpg');
+}
+
+function setup() {
+	var canvas = createCanvas(640, 360);
+	canvas.drawingContext.miterLimit = 2;
+}
+
+function draw() {
+	background('red');
+
+	textSize(20);
+	fill('white');
+	stroke(255);
+	strokeWeight(2);
+	textStyle(ITALIC); 
+	textAlign(CENTER)
+	text('Press anywhere on the red box (if you dare)', width / 2, height / 2);
+
+	//image
+	// if (mouseIsPressed) {
+	// image(memeImage, x, y, width, height);
+
+	//image intensifies
+	if (mouseIsPressed) {
+	image(memeImage, x, y, width, height);
+	 x += random(-3, 3);
+	 y += random(-3, 3);
+	textSize(50);
+	fill('red');
+	stroke(255);
+	strokeWeight(10);
+	textStyle(ITALIC); 
+	textAlign(CENTER)
+	text('Omae wa mou SHINDEIRU', width / 2, height - 300);
+	}
+}
+	
