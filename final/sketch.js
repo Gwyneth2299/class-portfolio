4adceaeaca383beb1b393f8056dcacec0eb652fb
Gwyneth2299 <70980984+Gwyneth2299@users.noreply.github.com>
2020-12-09 22:00:14 -0500

    update to sketch.js

---
+++ final/sketch.js
@@ -0,0 +1,102 @@
+/* Hit the Dot Kid-friendly version 
+  Final Project
+*/
+
+var r, g, b;
+var x, y;
+var score = 0;
+var time = 0;
+var endtime = 600;
+var gameOver = false;
+//var music;
+//var endmusic;
+
+// image
+function preload() {
+  memeImage = loadImage('meme.jpg');
+  /* music = loadSound('ding.mp3');                       //input music
+  endmusic = loadSound('buzzer.wav');
+  */
+  }
+
+
+// refresh screen for interaction
+function update() {
+ // music.play();
+  r = random(0, 100);
+  g = random(0, 20);
+  b = random(0, 200);
+  x = random(0 , width);
+  y = random(0 , height);
+}
+
+function setup() {
+  createCanvas(600, 450);
+  update();
+}
+
+function draw() {
+  background(r,g,b);
+  fill(random('white', 'red'));
+  ellipse(x ,y ,100);
+
+  // "try again" screen
+  if (time >= endtime) {
+  textSize(20);
+  fill('white');
+  text("Score: " + score, 10, 100);
+  text("Press the ENTER key to try again!", 10, 60); 
+
+   if (!gameOver) {
+  /*image(memeImage, x, y, width, height);
+   x += random(-3, 3);
+   y += random(-3, 3);
+  textSize(50);
+  fill('red');                                                  -image for meme patrick attempt-
+  stroke(255);
+  strokeWeight(10);
+  textStyle(ITALIC); 
+  textAlign(CENTER)
+  text('Omae wa mou SHINDEIRU', width / 2, height - 300);
+  */
+  }
+  gameOver = true;
+  } else {
+  time ++;
+  }
+  textSize(20);
+  fill('white');
+  text("Time: " + time + "/"+ endtime, 10,30);
+}
+
+function reset() {
+  gameOver = false;
+  time = 0;
+  score = 0;
+  update();
+}
+
+function keyPressed() {
+  if (keyCode == 13) { // Enter key
+    reset();
+  }
+}
+
+function mousePressed() {
+  var d = dist(mouseX, mouseY, x, y);
+  if (d < 50) {
+     if (time < endtime) {
+      update();
+      score ++;
+    } else {
+      reset();
+    } 
+
+  /*
+    add if statement for when red ellipse is clicked 
+  */  
+  }
+
+}
+
+
--- pattern_1/sketch.js
+++ pattern_1/sketch.js
@@ -1,10 +1,12 @@
 /*
-	pattern version 1
-	javascript loops
+	pattern version 2
+	generative pattern
+	using random
 */
 
 function setup() {
 	createCanvas(640, 360);
+	rectMode(CENTER);
 	pattern();
 }
 
@@ -12,28 +14,56 @@ function mousePressed() {
 	pattern();
 }
 
-
 function pattern() {
-	background(220);
+	background('#1F2F5D');
 
-	// landscape
-
-	//mountains
-	for (let x = 0; x < width; x+=100) {
-
+	// mountains
+	for (let x = random(-200); x < width; x += random(100, 200)) {
+		// brown
 		let r = random(50, 82);
 		let g = random(40, 65);
 		let b = random(30, 51);
-		fill(r, g, b); //brown
+		fill(r, g, b); 
 		noStroke();
 		let y = random(50, 200);
 		let w = random(200, 300);
-		rect(x, height - 250, x + 100, height);
+		triangle(x, height, x + w/2, y, x + w, height);
 	}
 
-	//trees
-	for (let x = 0; x < width; x += random(25, 50)) {
+	// trees
+	for (let x = 0; x < width; x += random(5, 100)) {
 		fill('rosybrown');
-		rect(x, height - 50, 10, 100);
+		let y = random(50);
+		let w = random(4, 10);
+		let h = random(50, 100);
+		rect(x, height - y, w, h);
+
+		// random green
+		var r = random(50);
+		var g = random(50, 200);
+		var b = random(50);
+		
+		fill(r, g, b);
+		let treeWidth = random(10, 40);
+		let treeHeight = random(treeWidth, treeWidth * 2);
+		ellipse(x, height - y - h/2, treeWidth, treeHeight);
 	}
+
+
+	// snow
+	//var snow = random(3, 10);
+	for (let x = 0; x < width; x += 20) {
+		// let x = random(width);
+		let y = random(350);
+		let w = random(10, 7);
+		let h = random(10, 7);
+		fill('white');
+		ellipse(x, y, w, h);
+	}
+
+		/* var clusterNumber = random(2,4);
+		for (let j = 0; j < clusterNumber; j++) {
+			ellipse(x + random(-20, 20), y + random(-5, 5), w, h);
+		}
+		*/
 }
