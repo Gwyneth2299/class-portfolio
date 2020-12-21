fcd4d7ba0ec4d014ba4b3b2cdd0a7dda3a6521a6
Gwyneth2299 <70980984+Gwyneth2299@users.noreply.github.com>
2020-12-20 23:55:46 -0500

    update sketch.js

--- pattern_1/sketch.js
+++ pattern_1/sketch.js
@@ -29,37 +29,59 @@ function pattern() {
 	fill('#9D625C');
 	quad(500, 200, 550, 100, 550, 360, 500, 360);
 
-	// lights
-	circle(0, 10, 10);
+	//windows x constant
+	fill('#FFF1B7');
+	for (let y = 170; y < 391; y += 110) {
+	rect(600, y, 60, 80);
+	}
 
-	for (let x = 0; x < width; x += 20) {
-		let r = random(0,200);
-		let g = random(0,200);
-		let b = random(0,200);
+	// windows left building attempt
+	//(but I can't do math rn so I'm sorry professor I'll try to figure this in my own time--
+	// thank you for being awesome I hope you see this note. Happy Holidays! Stay safe out there!)
+
+	//update nevermind I actually did it lol but Happy Holidays nonetheless!
+	fill('#FFF1B7');
+	for (let y = 30; y < 391; y += 110) {
+	quad(0, y, 50, y + 30, 50, y + 120, 0, y + 90);
+	quad(80, y + 50, 130, y + 80, 130, y + 170, 80, y + 140);
 
 	}
+
 	
 
-/*
-	// trees
-	for (let x = 0; x < width; x += random(5, 100)) {
-		fill('rosybrown');
-		let y = random(50);
-		let w = random(4, 10);
-		let h = random(50, 100);
-		rect(x, height - y, w, h);
-
-		// random green
-		var r = random(50);
-		var g = random(50, 200);
-		var b = random(50);
-		
-		fill(r, g, b);
-		let treeWidth = random(10, 40);
-		let treeHeight = random(treeWidth, treeWidth * 2);
-		ellipse(x, height - y - h/2, treeWidth, treeHeight);
+	// lights for left building
+	fill('red');
+	let y = 0;
+	for (let x = 0; x < 151; x += 15) {
+		ellipse(x, y, 17, 19);
+		y += 10;
+		let r = random(60, 255);
+		let g = random(60, 255);
+		let b = random(60, 255);
+		fill(r, g, b); 
+		noStroke();
 	}
-*/
+
+	//lights for right building y constant
+	for (let x = 550; x < 650; x += 16) {
+		ellipse(x, 100, 17, 19);
+		let r = random(60, 255);
+		let g = random(60, 255);
+		let b = random(60, 255);
+		fill(r, g, b); 
+		noStroke();
+	}
+	
+	// lights for right building
+	for (let x = 500; x < 560; x += 10) {
+		ellipse(x, y + 75, 17, 19);
+		y -= 17;
+		let r = random(60, 255);
+		let g = random(60, 255);
+		let b = random(60, 255);
+		fill(r, g, b); 
+		noStroke();
+	}
 
 	// snow
 	//var snow = random(3, 10);
